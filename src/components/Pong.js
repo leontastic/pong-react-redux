d708b855a0fbc17fb90d35281d48b96d5a64e2da
Leon Li <la2li@uwaterloo.ca>
2017-06-01 08:43:05 -0400

    normalize coordinates to center origin

--- src/components/Pong.js
+++ src/components/Pong.js
@@ -1,20 +1,31 @@
 import Radium from 'radium'
 import React, { Component } from 'react'
-import { Layer, Circle, Stage } from 'react-konva'
+import { Layer, Circle, Rect, Stage } from 'react-konva'
 import { connect } from 'react-redux'
 import multidecorator from 'react-multidecorator'
 import { Observable, Scheduler } from 'rx-dom'
+import Vector from 'victor'
 
 import { setGamePaused, animateGameState } from '../store/actions'
 import makeDispatcher from '../store/helpers/makeDispatcher'
 
-const Ball = ({ x, y }) => <Circle x={x} y={y} radius={5} fill='#a40000' />
+const Ball = ({ x, y, radius }) => <Circle x={x} y={y} radius={radius} fill='#a40000' />
+
+const Paddle = ({ x, y, width, height }) => <Rect x={x} y={y} width={width} height={height} fill='black' />
 
 class Pong extends Component {
   constructor () {
     super()
 
     this.handleKeyPress = this.handleKeyPress.bind(this)
+  }
+
+  fromCenter (vector, width = 0, height = 0) {
+    vector = Vector.fromObject(vector)
+    const { width: containerWidth, height: containerHeight } = this.props
+    const origin = new Vector(containerWidth / 2, containerHeight / 2)
+    const offset = new Vector(width / 2, height / 2)
+    return vector.add(origin).subtract(offset).toObject()
   }
 
   handleKeyPress (event) {
@@ -57,7 +68,9 @@ class Pong extends Component {
       <div>
         <Stage width={width} height={height}>
           <Layer>
-            <Ball {...ballPosition} />
+            <Ball radius={5} {...this.fromCenter(ballPosition, 5, 5)} />
+            <Paddle width={100} height={20} {...this.fromCenter(paddle1Position, 100, 20)} />
+            <Paddle width={100} height={20} {...this.fromCenter(paddle2Position, 100, 20)} />
           </Layer>
         </Stage>
         <pre style={{ backgroundColor: '#eee' }}>
